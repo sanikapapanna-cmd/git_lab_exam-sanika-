d399bb36d8beadf61d692b5cf72ee4252834f924
sanikapapanna-cmd <sanikapapanna@gmail.com>
2026-01-20 09:08:30 +0530

    programs added

---
+++ lab_exam/program.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+    int a,b;
+    int temp;
+    printf("the values are :",a,b);
+
+    temp=a;
+    a=b;
+    b=temp;
+    printf("the values are:",a,b);
+
+    
+
+}
